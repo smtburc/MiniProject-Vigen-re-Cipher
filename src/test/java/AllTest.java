7c2cf69979a0a797b17f6a0aa41aeef2aceb69b3
smtburc <sametburc@gmail.com>
2020-07-03 22:49:35 +0300

    first commit

---
+++ src/test/java/AllTest.java
@@ -0,0 +1,13 @@
+/**
+ * Print out total number of babies born, as well as for each gender, in a given CSV file of baby name data.
+ *
+ * @author Samet Burç
+ */
+
+import org.junit.Test;
+
+public class AllTest {
+
+
+
+}
